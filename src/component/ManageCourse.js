8cf468dab25f62e85cae73dc3393722033658e30
Wealth Iduwe <wealthiduwe@gmail.com>
2021-09-20 17:47:56 +0100

    Corrected post and put request using axios

--- src/component/ManageCourse.js
+++ src/component/ManageCourse.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
 import { toast } from "react-toastify";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 
 const ManageCourse = (props) => {
@@ -14,6 +14,7 @@ const ManageCourse = (props) => {
     Category: "",
   });
 
+  let history = useHistory();
   function handleValue({ target }) {
     setValue({
       ...value,
@@ -24,7 +25,7 @@ const ManageCourse = (props) => {
   const { id } = useParams();
   let baseUrl = `http://localhost:5000/courses/${id}`;
   // const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`;
-  const Url = "http://localhost:5000/courses";
+  // const Url = "http://localhost:5000/courses";
   useEffect(() => {
     axios
       .get(baseUrl)
@@ -41,21 +42,50 @@ const ManageCourse = (props) => {
 
   function handleSubmit(e) {
     e.preventDefault();
-    // if (!FormIsValid()) return;
-    fetch(Url + (value.id || ""), {
-      method: value.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.,
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify(value),
-    })
-      .then((resp) => resp.json())
-      .then((data) => setValue(data))
-      .then(() => {
-        props.history.push("/courses");
-        value.id
-          ? toast.success("Course Updated.")
-          : toast.success("Course Added.");
+
+    if (id) {
+      axios({
+        method: "PUT",
+        url: `http://localhost:5000/courses/${id}`,
+        data: {
+          Title: value.Title,
+          Author: value.Author,
+          Category: value.Category,
+        },
       })
-      .catch((error) => console.log(error));
+        .then(() => {
+          history.push("/courses");
+          toast.success("Course Updated.");
+        })
+        .catch((error) => console.log(error));
+    } else {
+      axios({
+        method: "POST",
+        url: "http://localhost:5000/courses",
+        data: value,
+      })
+        .then(() => {
+          history.push("/courses");
+          toast.success("Course Added.");
+        })
+        .catch((error) => console.log(error));
+    }
+
+    // if (!FormIsValid()) return;
+    // fetch(Url + (value.id || ""), {
+    //   method: value.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.,
+    //   headers: { "Content-type": "application/json" },
+    //   body: JSON.stringify(value),
+    // })
+    //   .then((resp) => resp.json())
+    //   .then((data) => setValue(data))
+    //   .then(() => {
+    //     props.history.push("/courses");
+    //     value.id
+    //       ? toast.success("Course Updated.")
+    //       : toast.success("Course Added.");
+    //   })
+    //   .catch((error) => console.log(error));
   }
 
   return (
